b090eb5242b66a19ee1e8ac6dcce869597749ae0
barx <barx1589@gmail.com>
2017-07-03 01:15:26 -0300

    se agrega efecto scroll para cada bloque, editando html, css y js

--- public/js/funciones.js
+++ public/js/funciones.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+	/* -- efecto parallax -- */
 	$(window).scroll(function(){
 		var barra = $(window).scrollTop();
 		var posicion =  (barra * 0.15);
@@ -6,4 +8,30 @@ $(document).ready(function(){
 			'background-position': '0 -' + posicion + 'px'
 		});
 	});
+
+	/* -- efecto scroll -- */
+	$('a#btn_inicio').click(function() {
+  		$('html, body').animate({
+    		scrollTop: $("div#box_uno").offset().top
+  		}, 2000)
+	});	
+
+	$('a#btn_nosotros').click(function() {
+  		$('html, body').animate({
+    		scrollTop: $("section#box_dos").offset().top
+  		}, 2000)
+	});
+
+	$('a#btn_servicio').click(function() {
+  		$('html, body').animate({
+    		scrollTop: $("section#box_tres").offset().top
+  		}, 2000)
+	});
+
+	$('a#btn_contacto').click(function() {
+  		$('html, body').animate({
+    		scrollTop: $("section#box_cuatro").offset().top
+  		}, 2000)
+	});		
+
 });
